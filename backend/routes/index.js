bcdccc53c7133fb5f578107d40b1ccee818310d2
programmerdavvy <ajayidavidbolarinde1@gmail.com>
2023-07-14 09:54:12 +0100

    intial commit

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express.Router();
-
-require("./endpoints/product")(app);
-require("./endpoints/user")(app);
-require("./endpoints/category")(app);
-
-
-module.exports = app; 
--- frontend/src/@fake-db/index.js
+++
@@ -1,9 +0,0 @@
-import mock from './mock'
-
-
-import './eCommerce'
-import './userList'
-
-
-mock.onAny().passThrough()
- 
